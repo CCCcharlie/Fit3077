e512444540ae51827901f5b1f59c1c18f60c1227
Daniel Gower-Hall <dgow0002@student.monash.edu>
2024-04-22 00:40:06 +1000

    Cleaned up utils.

--- Sprint2/Engine/src/fit3077engine/Utils/settings.py
+++ Sprint2/Engine/src/fit3077engine/Utils/settings.py
@@ -1,6 +1,4 @@
 from __future__ import annotations
-from typing import Type
-from pygame import Surface, display
 import pygame
 
 from ..ECS.scene import Scene
@@ -8,22 +6,21 @@ from ..ECS.scene import Scene
 
 class Settings:
 
-    screen: Surface = None
-    clock: pygame.time.Clock = None
-    scene: Scene = None
+    instance: Settings | None = None
 
-    def __init__(self) -> None:
-        raise TypeError("Cannot instantiate a Settings object, instead use statically")
-
-    @classmethod
-    def setup(
-        cls, *, screen_width: int = 1280, screen_height: int = 760, scene: Scene = None
+    def __init__(
+        self, scene: Scene, *, screen_width: int = 1280, screen_height: int = 760
     ) -> None:
-        cls.screen = display.set_mode((screen_width, screen_height))
-        cls.clock = pygame.time.Clock()
-        cls.scene = scene if scene is not None else Scene()
+        if self.instance is not None:
+            raise ValueError(
+                "Cannot instantiate singleton Settings more than once. Use get_instance()"
+            )
+        self.instance = self
+        self.scene = scene
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        self.clock = pygame.time.Clock()
 
-    @classmethod
-    def update_scene(cls, scene: Scene) -> Type["Settings"]:
-        cls.scene = scene
-        return cls
+    def get_instance(self) -> Settings:
+        if self.instance is None:
+            self.instance = Settings(Scene())
+        return self.instance
--- Sprint2/Engine/src/fit3077engine/Utils/utils.py
+++ Sprint2/Engine/src/fit3077engine/Utils/utils.py
@@ -1,24 +1,15 @@
 import pygame
-from ..ECS.components import ImageComponent, PositionComponent
-from ..ECS.entity import Entity
 from ..Events.handlers import PygameEventHandler
 
 from ..ECS.scene import Scene
 from .settings import Settings
 
 
-def game_loop(screen_width: int = 1280, screen_height: int = 760, fps: int = 60):
+def game_loop(
+    scene: Scene, screen_width: int = 1280, screen_height: int = 760, fps: int = 60
+):
     pygame.init()
-    Settings.setup(screen_width=screen_width, screen_height=screen_height)
-    ### TEMP
-    scene = Scene()
-    ent = Entity()
-    ent.add_component(ImageComponent("./assets/test.png")).add_component(
-        PositionComponent(30, 45)
-    )
-    scene.add_entity(ent)
-    ### END TEMP
-    Settings.update_scene(scene)
+    settings = Settings(scene, screen_width=screen_width, screen_height=screen_height)
 
     while True:
         # Handle all Pygame Events
@@ -26,8 +17,8 @@ def game_loop(screen_width: int = 1280, screen_height: int = 760, fps: int = 60)
             handler.handle_events()
 
         # Update Scene, Entities and Components
-        Settings.scene.update()
+        settings.scene.update()
 
         # Update Pygame display and wait for next frame
         pygame.display.update()
-        Settings.clock.tick(fps)
+        settings.clock.tick(fps)
