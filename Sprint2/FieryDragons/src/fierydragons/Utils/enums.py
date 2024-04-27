365b16761c53a4d60cd36e986233909bf66c5462
Daniel Gower-Hall <dgow0002@student.monash.edu>
2024-04-27 20:46:24 +1000

    Some helper classes and Enums for game board construction

---
+++ Sprint2/FieryDragons/src/fierydragons/Utils/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class Side(Enum):
+
+    TOP = 0
+    RIGHT = 1
+    BOTTOM = 2
+    LEFT = 3
---
+++ Sprint2/FieryDragons/src/fierydragons/Utils/helper_classes.py
@@ -0,0 +1,47 @@
+from typing import Iterator, Tuple
+
+from .enums import Side
+
+
+class SegmentedSquareIterator(Iterator[Tuple[int, int, Side]]):
+
+    def __init__(self, x: int, y: int, size: int, segments: int) -> None:
+        self.x = x
+        self.y = y
+        self._size = size
+        self.segments = segments
+        self._segment = 0
+        self._segments_placed = 0
+
+        self._segments_per_side = ((segments - 2) // 4) + 2
+        self.size = size // self._segments_per_side
+        self.side = Side.TOP
+
+    def __iter__(self) -> Iterator[Tuple[int, int, Side]]:
+        return self
+
+    def __next__(self) -> Tuple[int, int, Side]:
+        if self._segments_placed == self.segments:
+            raise StopIteration()
+
+        result = (self.x, self.y, self.side)
+
+        # Update Values
+        if self._segment < self._segments_per_side - 1:
+            self._segment += 1
+        else:
+            self._segment = 1
+            self.side = Side((self.side.value + 1) % len(Side))
+
+        match self.side:
+            case Side.TOP:
+                self.x += self.size
+            case Side.RIGHT:
+                self.y += self.size
+            case Side.BOTTOM:
+                self.x -= self.size
+            case Side.LEFT:
+                self.y -= self.size
+
+        self._segments_placed += 1
+        return result
