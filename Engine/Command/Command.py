6e2dd3dadba11b1149bef33c78f83171b1982387
Xavier Younan <xyounan@outlook.com>
2024-05-09 21:42:12 +1000

    reset master

--- Engine/Command/Command.py
+++
@@ -1,6 +0,0 @@
-#https://www.patternsgameprog.com/discover-python-and-patterns-20-better-commands
-# this is basically actions from FIT2099
-
-class Command():
-  def run(self):
-    raise NotImplementedError()
--- Engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Entity import Entity
-from .World import World
-from .Component import *
-from .Objects import * 
-from .Input import Input
