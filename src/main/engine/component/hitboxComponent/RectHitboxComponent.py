cf96e8342bbfa4fe62feb74cd9f82c3ab3a2fbf8
Xavier Younan <xyounan@outlook.com>
2024-05-11 15:31:15 +1000

    Add rect hitbox component

---
+++ src/main/engine/component/hitboxComponent/RectHitboxComponent.py
@@ -0,0 +1,56 @@
+from pygame import Surface, Rect, draw
+from src.main.engine.utils.Vec2 import Vec2
+from src.main.engine.component.TransformComponent import TransformComponent
+from src.main.engine.component.hitboxComponent.HitboxComponent import HitboxComponent
+
+
+class RectHitboxComponent(HitboxComponent):
+  """
+  Rectangle hitbox component
+  
+  For now the scaling and rotation doesn't work
+  """
+  def __init__(self, transformComponent: TransformComponent, width: int, height: int, debug=False):
+    """
+    Create the hitbox
+    
+    Args:
+      transformComponent (TransformComponent): The transform this hitbox is attached to
+      width (int): The width of the hitbox
+      height (int): The height of the hitbox
+      debug (bool): If True will render the hitbox shape
+    """
+    super().__init__(transformComponent, debug)
+    self.__width = width
+    self.__height = height
+
+  @property
+  def width(self) -> int:
+    return self.__width
+  
+  @width.setter
+  def width(self, value):
+    self.__width = value
+
+  @property
+  def height(self) -> int:
+    return self.__height
+  
+  @height.setter
+  def height(self, value: int):
+    self.__height = value
+
+  def _checkPointCollision(self, position: Vec2, scale: Vec2, rotation: int, point: Vec2) -> bool:
+    ##ignore scale and rotation for now
+    xInBounds = position.x <= point.x <= position.x + self.__width
+    yInBounds = position.y <= point.y <= position.y + self.__height
+
+    return xInBounds and yInBounds
+
+
+  def _drawDebug(self, displaySurf: Surface, position: Vec2, scale: Vec2, rotation: int):
+    rect = Rect(position.x, position.y, self.__width, self.__height )
+    draw.rect(displaySurf, (255,0,0), rect, width=3)
+
+    
+  
--- src/main/engine/component/hitboxComponent/__init__.py
+++ src/main/engine/component/hitboxComponent/__init__.py
@@ -1,3 +1,4 @@
 # engine/component/hitboxComponent/__init__.py
 from .CircleHitboxComponent import CircleHitboxComponent
 from .HitboxComponent import HitboxComponent
+from .RectHitboxComponent import RectHitboxComponent
