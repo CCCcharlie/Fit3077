f5f57ea540c67eaaf2b673b7b7ccfad130a26d18
dgow0002 <dgow0002@student.monash.edu>
2024-05-13 06:47:50 +1000

    Remove irrelevant imports, add offset

--- FieryDragons/src/fieryDragons/utils/CircleCoordinateIterator.py
+++ FieryDragons/src/fieryDragons/utils/CircleCoordinateIterator.py
@@ -1,25 +1,17 @@
-from typing import Iterator, Tuple
-from math import pi, sin, cos, acos
-from engine.component.TransformComponent import TransformComponent
-from engine.component.renderable.RectComponent import RectComponent
-from engine.entity.Entity import Entity
-from engine.scene.Scene import Scene
-from random import randint
+from typing import Iterator 
+from math import pi, sin, cos 
 
-
-from engine.scene.World import World
 from engine.utils.Vec2 import Vec2
-from pygame import Color
 
 class CircleCoordinateIterator(Iterator[Vec2]):
 
-    def __init__(self, elements : int, radius : int, center_x : int, center_y : int) -> None:
+    def __init__(self, elements : int, radius : int, center_x : int, center_y : int, offset : int = 0) -> None:
         self.size = (radius * 2) // (elements // 2) 
         self.__center_x = center_x
         self.__center_y = center_y
         self.__elements = elements
         self.__n = 0 
-        self.__radius = radius - self.size # Center point exists on the inner circle
+        self.__radius = radius - self.size * (offset + 1) # Center point exists on the inner circle
 
     def __iter__(self) -> Iterator[Vec2]:
         return self
