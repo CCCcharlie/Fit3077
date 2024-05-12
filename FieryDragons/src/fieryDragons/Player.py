b66ecea58b0fe1ae0113bf1e308587bfc961293b
Xavier Younan <xyounan@outlook.com>
2024-05-12 21:03:55 +1000

    Initial logic for Player move

--- FieryDragons/src/fieryDragons/Player.py
+++ FieryDragons/src/fieryDragons/Player.py
@@ -1,11 +1,32 @@
 
 
 from fieryDragons.Segment import Segment
+from fieryDragons.enums.AnimalType import AnimalType
 
 
 class Player:
   def __init__(self, startingSegment: Segment):
     self.position = 0
     self.path = startingSegment.generatePath(startingSegment)
+
+  def move(self, animalType: AnimalType, amount: int):
+    if animalType is AnimalType.PIRATE_DRAGON:
+      newLocation = self.position - amount
+      # move there
+      # end turn
+
+  
+    if animalType is self.path[self.position].getAnimalType():
+      newLocation = self.position + amount
+      # move there
+
+    #end turn
+
+
+
+    
+
+    
+    
   
 
