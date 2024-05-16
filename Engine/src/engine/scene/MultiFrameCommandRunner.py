c09ea928369109e9ec1129e3a6849f3382381599
Xavier Younan <xyounan@outlook.com>
2024-05-16 17:44:23 +1000

    Add commenting to MultiFrameCommandRunner, add logic to call the runner in World, move LinearMoveMFCommand to engine

--- Engine/src/engine/scene/MultiFrameCommandRunner.py
+++ Engine/src/engine/scene/MultiFrameCommandRunner.py
@@ -7,12 +7,33 @@ from engine.utils.SingletonMeta import SingletonMeta
 
 class MultiFrameCommandRunner(metaclass=SingletonMeta):
   def __init__(self):
+    """
+    Create the multiframe command runner
+    """
     self.__commands: List[MultiFrameCommand] = []
   
   def addCommand(self, c: MultiFrameCommand):
+    """
+    Add a command to the multi frame command runner
+    
+    Args:
+      c (MultiFrameCommand): the command to add
+    """
     self.__commands.append(c)
 
+  def clear(self):
+    """
+    Delete all current commands
+    """
+    self.__commands = []
+
   def update(self, dt: float):
+    """
+    Run the update method on all commands and destroy those who are done
+
+    Args:
+      dt (float): the time in milliseconds since the last update
+    """
     # run all commands
     for command in self.__commands:
       if command.isRunning():
