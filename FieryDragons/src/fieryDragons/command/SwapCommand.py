ede195b77ea338544f398d1c2d3b02b9887542e0
xyou0003 <xyou0003@student.monash.edu>
2024-06-02 14:30:15 +1000

    Swap chit card has been created

---
+++ FieryDragons/src/fieryDragons/command/SwapCommand.py
@@ -0,0 +1,17 @@
+from engine.command.Command import Command
+
+
+
+class SwapCommand(Command):
+    """
+    Shuffle the ChitCards by rearranging the order
+    """
+    def __init__(self):
+        pass
+
+    def run(self):
+        print("Swapping")
+
+
+
+        
